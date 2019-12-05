28d97202b408e3373941c7ebd0370d85ccc2720b
Jens Bäckvall <jensbackvall@t-nova.org>
2019-12-05 12:30:14 +0100

    Update database.sql

--- sql-creation/database.sql
+++ sql-creation/database.sql
@@ -17,6 +17,7 @@ nProductId int IDENTITY Primary key,
 cName varchar(255) not null,
 cDescription varchar(2048),
 nUnitPrice money not null,
-nStock int not null
+nStock int not null,
+bFragile bit not null
 )
 

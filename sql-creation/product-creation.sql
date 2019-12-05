30c1bc00a8a110c3fc3580e2fbd04a168c2745f3
Jens Bäckvall <jensbackvall@t-nova.org>
2019-12-05 09:55:34 +0100

    Create product-creation.sql

---
+++ sql-creation/product-creation.sql
@@ -0,0 +1,95 @@
+
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice,  nStock, nMinimumStock)
+VALUES ('Blue Shoes ', 'The best shoes for wearing, when all your clothes are blue. They automatically change colour to fit your specific shade of blue ', 728.45,165, 10);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Banana Boat ', 'This boat will cater to all your seafaring needs. But only if picking water-bananas is involved ', 762000.00,3, 2);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Premium Banana', 'a long curved fruit which grows in clusters and has soft pulpy flesh and yellow skin when ripe', 2, 2000000,500);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('F for Effort: More of the Very Best Totally Wrong Test Answers', 'From the author of the national bestseller F in Exams comes a brand new compendium of hilarious and inventive wrong test answers and homework hiccups. Featuring gems from elementary school ("two halves make a whale"), middle grades (Q: What does "germinate" mean? A: To become a German citizen), and high school (Q: Fossil fuels are usually associated with which major type of rock? A: Classic rock), these 250 examples of creative invention are sure to charm anyone who has had to bluff or blunder their way through a test. Plus, this is a fixed-format version of the book, which looks nearly identical to the print version. ', 60, 120,10);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Bunch of Bananas ', '473 ripe bananas in a bunch. Ready to eat when we ship it, so you had better be ready to eat them when you receive them!!! ', 376.23, 76, 30);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES (' Brita Grand Water Filter Pitcher, Red, 10 Cup', 'Container Size: 80 oz. Material: Plastic Color: Red ', 35, 45, 5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Bath Water ', 'A bottle of bath water, guaranteed to have touched the cat that bathed in it. This cat is 3 months old and has blue-ish grey fur. It answers to the name of Pappacallaho ', 10.99, 67,10);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('iPhone 2, non-functional ', 'A phone that has seen better days. It wont work, and we guarantee that the screen is broken. Be aware that tiny shards of glass may cut your fingers as you open the box. ', 1000.72, 89, 5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Toxic Waste Yellow Drum W/ sour Candy ', ' ', 20, 100,10);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Genuine Light Saber', 'This Light Saber is real, and you are able to slice your androids like butter, never before has it been as fun to go meet the younglings', 5000000, 5,1);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Gymnastic shorts ', 'Shorts that perform gymnastics for you, so that you dont need to perform yourself. Just throw them into the gym-hall, and watch them jumping and doing summersaults. Endless hours of fun! ', 997.32, 6, 3);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Long Play Coder ', 'This coder will code for you for a loooooooooong time, performing well on any and all tasks given to him. But only on Linux and Windows XP systems. He answers to the name of LP ', 10110.01, 1,1);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Stonie Coder ', 'This coder can be used when creating Minecraft servers. Just plug him into a continuous drip of 1992 Jolt cola and watch him go. Endless hours of fun. He answers to the name DJ ', 101101.11, 1, 1);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Clone Army ', 'Have you ever had any issues attacking or defending the galaxy, look no further, the product is produced by Sifo-Dyas and are ready to destroy all your enemies and worries ', 0, 0,0);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Yodaa doll ', 'This little doll is cute and cuddly and vaguely resembles Y**a from a copyrighted movie named S*** W***. We have no rights to this product or anything from the affiliated universe, so buy it at your own peril. It is shipped directly from the factory where original merchandise for this well known franchise is produced. ', 19.78, 1978, 19);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Banana Water ', 'This water is mixed with premium bananas from the Banana Islands, just south of Puerto Ventura Ricolada, the capital of Wendurandela. The bananas are lovingly squished between a live horses thighs, and then sieved into pure spring water that has been left to ferment for 27 days in the hot Wendurandelan sun. Drink it and be surprised! ', 129.12, 1000, 150);
+
+
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Love ', 'Are you looking for love? This product is pure love in a jar. Just open it and feel it gushing out at you. You will feel loved for a long time… or at least 10 seconds. Whichever comes first. ', 69.96, 99, 50);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Banana meatloaf', 'You will never want normal meatloaf again', 25, 21,5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Apple', 'Not an electronic company, but the delicious fruit', 5, 456,50);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Premium banana plane', 'Premium plane made out of bananas, runs on monkeys(not included) ', 50000, 3,1);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Banana phone', 'A phone and a banana in 1, dont eat it until your done talking! ', 999, 7,3);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Banana hub', 'A banana hub sure to be a-peel-ing to anyone with limited USB ports ', 25, 36,10);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Big Banana Cup', 'With great power comes great responsibility', 10, 76,10);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Das Star Wars Archiv. 1977–1983 ', 'Seit im Jahr 1977 zum ersten Mal der magische Einleitungssatz Es war einmal vor langer Zeit in einer weit, weit entfernten Galaxie ... über die Kinoleinwand flimmerte, sind Generationen von Film- und Scifi-Fans Luke Skywalker, Han Solo, Prinzessin Leia, R2-D2 und C-3PO und natürlich dem beliebtesten Schurken und schlimmsten Vater des Universums, Darth Vader, auf ihren abenteuerlichen Reisen durch die endlosen Weiten des Alls gefolgt, haben – je nach der individuellen sittlich-moralischen Grunddisposition – aufseiten der Rebellen oder der Sturmtruppen gekämpft, den Millenium Falken geschrottet, mit Meister Yoda die Syntax verunstaltet und das Lichtschwert tanzen lassen.', 900, 10,5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Tefal E 85604 J.Oliver Pfanne 24 cm ', '6-Lagen TEFAL Prometal Pro Antihaft-Versiegelung mit Hartgrundierung und Top Coat
+THERMO-SPOT: Der weltweit erste in die Pfanne integrierte Temperaturanzeiger! Er zeigt an, wann die Pfanne die optimale Brattemperatur erreicht hat.
+ ', 1000, 20,5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Der neue Amazon Echo (3. Generation), smarter Lautsprecher mit Alexa, Anthrazit Stoff ', 'Lernen Sie Echo kennen – Echo (3. Gen.) bietet neue, hochwertige Lautsprecher mit Dolby-Verarbeitung, die den Raum mit purem 360-Grad-Klang mit klaren Höhen und dynamischen Bässen füllen. ', 3000, 30,5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Single Malt Scotch Whisky Geschenk 1818 23-Karat Deutsches Blattgold - Schottischer Premium Whiskey - Geschenkidee für Männer ', 'Der Whisky Conde Lumar 1818 ist ein unvergleichlicher, extrem eleganter und aromatischer Whisky. Auf der Grundlage unserer Expertise und Erfahrung aus fast 200 Jahren haben wir ein einzigartiges Produkt geschaffen. Das Ergebnis ist ein Meisterwerk, das als Referenz für alles steht, was uns ausmacht: Geduld, Güte, Qualität und meisterhafte Handwerkskunst. ', 79, 20,5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Der König der Löwen – Neuverfilmung 2019', 'ist ein computeranimierter US-amerikanischer Kinofilm von Jon Favreau, produziert von ihm selbst, Karen Gilchrist und Jeffrey Silver. ', 89, 60,5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Air Force two ', 'If you think number one is good you will love number two! ', 5000000, 30,5);
+
+INSERT INTO TProduct (cName, cDescription, nUnitPrice, nStock, nMinimumStock)
+VALUES ('Trump Caps ', 'For those who love wall building and big integrity', 99, 100,5);

446c9616b3ec96f7442f416af8d4ab3dbe13c07a
Jens Bäckvall <jensbackvall@t-nova.org>
2019-12-05 12:09:25 +0100

    Update database.sql

--- sql-creation/database.sql
+++ sql-creation/database.sql
@@ -1,12 +1,22 @@
 --Create Database <databasename>
 
 
-Create Table TProduct
+Create Table TFood
 (
 nProductId int IDENTITY Primary key,
 cName varchar(255) not null,
 cDescription varchar(2048),
 nUnitPrice money not null,
 nStock int not null,
+dExpirationDate date not null
+)
+
+Create Table TNonFood
+(
+nProductId int IDENTITY Primary key,
+cName varchar(255) not null,
+cDescription varchar(2048),
+nUnitPrice money not null,
+nStock int not null
 )
 

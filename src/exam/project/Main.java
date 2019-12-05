5af82b5f9e2dc4f94c769433fd6a7a6f206a325b
Jakob Larsen <jslar28@gmail.com>
2019-12-05 10:22:45 +0100

    Added quick scanner example

--- src/exam/project/Main.java
+++ src/exam/project/Main.java
@@ -1,8 +1,12 @@
 package exam.project;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
 	// write your code here
         System.out.println("Enter number:");
         System.out.println("1: Create new product");
@@ -10,5 +14,8 @@ public class Main {
         System.out.println("3: Delete product");
         System.out.println("4: Receive product(s)");
         System.out.println("5: Ship product(s)");
+
+        String command = input.next();
+        System.out.println("You selected: " + command);
     }
 }

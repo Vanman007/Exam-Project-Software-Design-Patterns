21a9646d7773dbb26f8b8b8771cda911ece42cf0
Jakob Larsen <jslar28@gmail.com>
2019-12-05 10:20:48 +0100

    Added first menu

--- src/exam/project/Main.java
+++ src/exam/project/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Enter number:");
+        System.out.println("1: Create new product");
+        System.out.println("2: Edit existing product");
+        System.out.println("3: Delete product");
+        System.out.println("4: Receive product(s)");
+        System.out.println("5: Ship product(s)");
     }
 }

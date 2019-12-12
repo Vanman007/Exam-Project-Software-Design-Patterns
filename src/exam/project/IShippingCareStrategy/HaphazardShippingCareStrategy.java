c1cd9bc30b77d249c906b1451b4e4459c8e2373c
Jens Bäckvall <jens@entropyfox.com>
2019-12-12 15:35:24 +0100

    add IShippingCareStrategy
    Add Interface and Concrete implementations

---
+++ src/exam/project/IShippingCareStrategy/HaphazardShippingCareStrategy.java
@@ -0,0 +1,16 @@
+package exam.project.IShippingCareStrategy;
+
+public class HaphazardShippingCareStrategy implements IShippingCareStrategy {
+
+    private float careCostMultiplier;
+
+    @Override
+    public float CalculateShippingCare(int shippingMultiplier, float weight, float size) {
+        return 0;
+    }
+
+    public void setCareCostMultiplier(float careCostMultiplier) {
+        this.careCostMultiplier = careCostMultiplier;
+    }
+
+}

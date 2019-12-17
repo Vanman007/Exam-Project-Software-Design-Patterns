a896a15efc5df7a1ebe1077fd933c7cf56c6480d
vanman007 <tp_vanman@hotmail.com>
2019-12-17 07:27:46 +0100

    email service

---
+++ src/exam/project/JavaMailUtil.java
@@ -0,0 +1,46 @@
+package exam.project;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class JavaMailUtil {
+    public static void sendMail(String recepient) throws AddressException, MessagingException{
+        System.out.println("Sending Message");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.port","587");
+        String myAccountEmail= "noreply.swd.emailservice@gmail.com";
+        String password= "Gx62062TsjK";
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+        Message message = prepareMessage(session, myAccountEmail, recepient);
+        Transport.send(message);
+        System.out.println("Message Sent");
+    }
+
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient){
+        Message message = new MimeMessage(session);
+        try{
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("Content Title");
+            message.setText("hello world\nnew line");
+            return message;
+        } catch (Exception ex){
+            Logger.getLogger(JavaMailUtil.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+}

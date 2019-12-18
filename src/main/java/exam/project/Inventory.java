1199da459395a70f5232d52ea18c66e806e500ee
Jakob Olsen <Koen_jolle@hotmail.com>
2019-12-18 14:46:33 +0100

    The One With The Add And Remove Products

--- src/main/java/exam/project/Inventory.java
+++ src/main/java/exam/project/Inventory.java
@@ -15,11 +15,58 @@ public class Inventory implements ISubscriber {
     private ArrayList<MidEndTV> midEndTVs = new ArrayList<>();
     private ArrayList<DesignerTV> designerTVs = new ArrayList<>();
 
-    public void addProduct(String type) {
+    public void addProduct(ElectronicsProduct electronicsProduct, String type) {
+
+        switch (type){
+            case "Discount TV":
+                discountTVs.add((DiscountTV)electronicsProduct);
+            break;
+            case "Discount Radio":
+                discountRadios.add((DiscountRadio) electronicsProduct);
+            break;
+            case "MidEnd TV":
+                midEndTVs.add((MidEndTV) electronicsProduct);
+            break;
+            case "MidEnd Radio":
+                midEndRadios.add((MidEndRadio) electronicsProduct);
+            break;
+            case "Designer TV":
+                designerTVs.add((DesignerTV) electronicsProduct);
+            break;
+            case "Designer Radio":
+                designerRadios.add((DesignerRadio) electronicsProduct);
+            break;
+            default:
+                System.out.println("ERROR!");
+            break;
+        }
 
     }
 
     public void removeProduct(String type){
+        switch (type){
+            case "Discount TV":
+                discountTVs.remove(0);
+                break;
+            case "Discount Radio":
+                discountRadios.remove(0);
+                break;
+            case "MidEnd TV":
+                midEndTVs.remove(0);
+                break;
+            case "MidEnd Radio":
+                midEndRadios.remove(0);
+                break;
+            case "Designer TV":
+                designerTVs.remove(0);
+                break;
+            case "Designer Radio":
+                designerRadios.remove(0);
+                break;
+            default:
+                System.out.println("ERROR!");
+                break;
+        }
 
     }
 
--- src/main/java/exam/project/Products/ElectronicsProduct.java
+++ src/main/java/exam/project/Products/ElectronicsProduct.java
@@ -8,4 +8,23 @@ public abstract class ElectronicsProduct {
     protected int size;
     protected String brand;
 
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
 }
